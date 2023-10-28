97d8664efc7d7b2fb448b1156c71658f859f8e75
Massipssa Massipssa <kerrache.massipssa@gmail.com>
2023-10-28 16:59:18 +0000

    Add partitionning and bucketing

---
+++ spark/partitionning_bucketing.py
@@ -0,0 +1,27 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('partition-bucket-app').getOrCreate()
+
+df = spark.createDataFrame(
+    [("person_1", "20"), ("person_2", "56"), ("person_3", "89"), ("person_4", "20")],
+    ["name","age"]
+)
+
+# Partitionning
+df = df.repartition(4, "age")
+print(df.rdd.getNumPartitions())
+
+# Coalsce
+
+
+df.write.mode("overwrite").partitionBy("age").csv("data/output")
+
+
+# Bucketing 
+df.write.bucketBy(5, "age").saveAsTable("bucketed_table")
+
+
+df.write.bucketBy(10, "age")\
+    .sortBy("name")\
+    .saveAsTable("sorted_bucketed_table")
